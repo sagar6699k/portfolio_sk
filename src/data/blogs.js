2e591a6fe1cae0057ccfd9588387397b35442065
sagar6699k <sagarkurewar@gmail.com>
2023-10-06 16:53:21 +0530

    updated portfolio

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,24 +1,32 @@
-import Traveltech from  '../img/blogs/Traveltech.png';
-import zoomcar from  '../img/blogs/zoomcar.png';
+import Kickstart from '../img/kickstart.png';
+import scavenger from '../img/scavenger.png';
 
 const blogs = [
     {
         id: 1,
-        title: 'Building clone of the Zoomcar Website',
+        title: 'Clone of Nykaa.com',
         date: "03",
-        src:"https://medium.com/@Vishal_Patil/cloning-of-zoomcar-com-website-using-backend-591429b313e3",
+        src: "https://nykaa6699-sagarkurewar.hashnode.dev/cloning-the-e-commerce-website-nykaacom-using-backend",
         month: 'Oct 2021',
-        image: zoomcar
-       
+        image: "https://cdn.hashnode.com/res/hashnode/image/upload/v1633265154965/8gt2-R-FG.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp"
+
     },
     {
-        id: 2, 
-        title: 'Building TravelTech Website',
-        date: "09",
+        id: 2,
+        title: 'Kickstarter-Clone',
+        date: "19",
+        month: 'Dec 2021',
+        src: "https://medium.com/@shubh.110394/kickstarter-website-development-744de3a36c56",
+        image: Kickstart
+    },
+    {
+        id: 3,
+        title: 'scAvengers -The E-Waste Management App',
+        date: "13",
         month: 'Nov 2021',
-        src:"https://medium.com/@Vishal_Patil/vishal-patil-41854732ceff",
-        image: Traveltech
-    }
+        src: "https://medium.com/@pandeypushpendra3/scavengers-the-e-waste-management-app-afc39f79f4aa",
+        image: scavenger
+    },
 ]
 
 export default blogs;
--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,28 +1,49 @@
 // import css1 from '../img/portImages/cv_css.png';
-import p1 from '../img/portImages/1.png';
-import p2 from '../img/portImages/2.png';
+import p1 from '../img/scavenger.png';
+import p2 from '../img/Movix.png';
+import p3 from '../img/kickstart.png';
 
 const portfolios = [
     {
         id: 1,
         category: 'Web Application',
-        image: p1,
-        link1: 'https://github.com/vishalpatil12014017/Zoomcar_backend',
-        link2: 'https://zoomcarcom.herokuapp.com',
-        title: 'Zoomcar Clone',
-        text: 'In This Application we have used HTML,CSS,JavaScript,EJS to build this application.Also we are using Express Js to give Backend to this project',
-        text1: " : HTML | CSS | JavaScript | MongoDb | Express Js"
+        image: p2,
+        link1: 'https://github.com/sagar6699k/movix_app',
+        link2: 'https://movix-app-red.vercel.app/',
+        title: 'Movix App',
+        text: 'Movix is the web application, where user can search details of any movie or Tv shows like cast, ratings, also can watch trailers and related videos.',
+        text1: " : ReactJS | Redux-toolkit | react-player | sass"
     },
     {
         id: 2,
         category: 'Web Application',
-        image: p2,
-        link1: 'https://github.com/vishalpatil12014017/TravelTech',
-        link2: 'https://aazadi-traveltech.vercel.app/',
-        title: 'TravelTech',
-        text: 'In This Application we have used React Js,Bootstrap to build this application.Also we are using Express Js to give Backend to this project',
-        text1: " : React Js | Bootstrap | MongoDb | Express Js"
-    }
+        image: p1,
+        link1: 'https://github.com/sagar6699k/E-wasteManagement',
+        link2: 'https://e-waste-management.netlify.app/',
+        title: 'scAvengers -The E-Waste Management App',
+        text: 'scAvengers is an E-waste Management App that allows users to sell e-waste scrap at any price they want.',
+        text1: " : ReactJS | NodeJS | Express | Mongodb | CSS"
+    },
+    {
+        id: 2,
+        category: 'Web Application',
+        image: "https://grocurv.com/media/images/Nykaa.height-480.png",
+        link1: 'https://github.com/sagar6699k/Nykaa-clone',
+        link2: 'https://clonednykaa.herokuapp.com/',
+        title: 'Clone of Nykaa.com',
+        text: 'Nykaa which is very popular E-Commerce website which helps users to buy Cosmetics products and Beauty products online in India at best prices.',
+        text1: " : eJs | CSS | Javascript | Express Js | MongoDB"
+    },
+    {
+        id: 3,
+        category: 'Web Application',
+        image: p3,
+        link1: 'https://github.com/sagar6699k/Atom',
+        link2: 'https://atom-client.vercel.app/',
+        title: 'Kickstarter-Clone',
+        text: 'Kickstarter is a US based crowdfunding website, creators will share their ideas with the communities and the communities will fund them.',
+        text1: " : ReactJS | NodeJS | Express | Mongodb | CSS"
+    },
 ]
 
 export default portfolios;
