ab49078d397ff66318075cdba875cb2be2ac593b
Sagar <sagarkurewar@gmail.com>
2024-04-27 21:00:36 +0530

    updated it

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -3,6 +3,8 @@ import p1 from '../img/scavenger.png';
 import p2 from '../img/Movix.png';
 import p3 from '../img/kickstart.png';
 import p4 from '../img/NotesApp.png';
+import p5 from '../img/youtube.png';
+import p6 from '../img/weather_app.png';
 
 const portfolios = [
     {
@@ -25,35 +27,55 @@ const portfolios = [
         text: 'Movix is the web application, where user can search details of any movie or Tv shows like cast, ratings, also can watch trailers and related videos.',
         text1: " : ReactJS | Redux-toolkit | react-player | sass"
     },
-    {
-        id: 3,
-        category: 'Web Application',
-        image: p1,
-        link1: 'https://github.com/sagar6699k/E-wasteManagement',
-        link2: 'https://e-waste-management.netlify.app/',
-        title: 'scAvengers App',
-        text: 'scAvengers: An innovative e-waste management app empowering users to set their own prices when selling e-waste scrap.',
-        text1: " : ReactJS | NodeJS | Express | Mongodb | CSS"
-    },
-    {
-        id: 4,
-        category: 'Web Application',
-        image: "https://grocurv.com/media/images/Nykaa.height-480.png",
-        link1: 'https://github.com/sagar6699k/Nykaa-clone',
-        link2: 'https://clonednykaa.herokuapp.com/',
-        title: 'Clone of Nykaa.com',
-        text: 'Nykaa which is very popular E-Commerce website which helps users to buy Cosmetics products and Beauty products online in India at best prices.',
-        text1: " : eJs | CSS | Javascript | Express Js | MongoDB"
-    },
+    // {
+    //     id: 3,
+    //     category: 'Web Application',
+    //     image: p1,
+    //     link1: 'https://github.com/sagar6699k/E-wasteManagement',
+    //     link2: 'https://e-waste-management.netlify.app/',
+    //     title: 'scAvengers App',
+    //     text: 'scAvengers: An innovative e-waste management app empowering users to set their own prices when selling e-waste scrap.',
+    //     text1: " : ReactJS | NodeJS | Express | Mongodb | CSS"
+    // },
+    // {
+    //     id: 4,
+    //     category: 'Web Application',
+    //     image: "https://grocurv.com/media/images/Nykaa.height-480.png",
+    //     link1: 'https://github.com/sagar6699k/Nykaa-clone',
+    //     link2: 'https://clonednykaa.herokuapp.com/',
+    //     title: 'Clone of Nykaa.com',
+    //     text: 'Nykaa which is very popular E-Commerce website which helps users to buy Cosmetics products and Beauty products online in India at best prices.',
+    //     text1: " : eJs | CSS | Javascript | Express Js | MongoDB"
+    // },
+    // {
+    //     id: 5,
+    //     category: 'Web Application',
+    //     image: p3,
+    //     link1: 'https://github.com/sagar6699k/Atom',
+    //     link2: 'https://atom-client.vercel.app/',
+    //     title: 'Kickstarter-Clone',
+    //     text: 'Kickstarter is a US based crowdfunding website, creators will share their ideas with the communities and the communities will fund them.',
+    //     text1: " : ReactJS | NodeJS | Express | Mongodb | CSS"
+    // },
     {
         id: 5,
         category: 'Web Application',
-        image: p3,
-        link1: 'https://github.com/sagar6699k/Atom',
-        link2: 'https://atom-client.vercel.app/',
-        title: 'Kickstarter-Clone',
-        text: 'Kickstarter is a US based crowdfunding website, creators will share their ideas with the communities and the communities will fund them.',
-        text1: " : ReactJS | NodeJS | Express | Mongodb | CSS"
+        image: p6,
+        link1: 'https://github.com/sagar6699k/Weather-app',
+        link2: 'http://weatherapp-eta.vercel.app/',
+        title: 'Weather App',
+        text: 'Weather app delivers real-time forecasts with intuitive design and user experience.users can stay informed about upcoming forecasts.',
+        text1: " : ReactJS | JSX | CSS"
+    },
+    {
+        id: 6,
+        category: 'Web Application',
+        image: p5,
+        link1: 'https://github.com/sagar6699k/youtube_clone',
+        link2: 'https://sagar6699k.github.io/youtube_clone/',
+        title: 'Youtube Clone',
+        text: 'ReactJS-powered weather app delivers real-time forecasts with intuitive design and seamless user experience.',
+        text1: " : ReactJS | JSX | CSS"
     },
 ]
 
